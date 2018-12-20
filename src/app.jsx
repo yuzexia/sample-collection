7bd11cdb43535a7b894d1b9f6a052025629628b8
yuze.xia <327932801@qq.com>
2018-12-21 01:46:04 +0800

    loaders

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,24 @@
  * @Author: yuze.xia 
  * @Date: 2018-12-20 16:53:00 
  * @Last Modified by: yuze.xia
- * @Last Modified time: 2018-12-21 00:16:37
+ * @Last Modified time: 2018-12-21 01:20:16
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+import 'font-awesome/css/font-awesome.min.css';
+
 // 引入css文件
 import './index.css';
+import './index.scss';
+
 
 ReactDOM.render(
-    <h1>hello webpack</h1>,
+    <div>
+        <h1>hello webpack</h1>,
+        <i className="fa fa-address-book"></i>
+    </div>,
     document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: './src/app.jsx', //入口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'js/app.js'
     },
     module: {
         rules: [
+            // js，jsx文件的处理
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
@@ -19,18 +22,64 @@ module.exports = {
                     }
                 }
             },
+            // css文件的处理
             {
                 test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            // sass文件的处理
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            // 图片配置
+            {
+                test: /\.(png|jpg|gif)$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            // 字体文件的配置
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
                 ]
             }
         ]
     },
     plugins: [ // 插件
+        // 处理html文件
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        // 独立css文件
+        new ExtractTextPlugin('css/[name].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
         })
-    ]
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
 };
